991c22eed2c6c4b0472283ca18f1395865b9f229
OkaPmna <okapramn092@gmail.com>
2026-02-21 15:17:38 +0800

    update db connection env

--- src/config/koneksi.php
+++ src/config/koneksi.php
@@ -1,9 +1,18 @@
 <?php
-$host = "db";
-$user = "user_app";
-$pass = "password_app"; // default XAMPP kosong
-$db   = "unimq";
+$envPath = __DIR__ . '/../.env';
 
+if (file_exists($envPath)) {
+    $env = parse_ini_file($envPath);
+    
+    $host = $env['DB_HOST'] ?? 'db';
+    $user = $env['DB_USER'] ?? 'user_app';
+    $pass = $env['DB_PASS'] ?? 'password_app';
+    $db   = $env['DB_NAME'] ?? 'unimq';
+} else {
+    die(".env file not found");
+}
+
+// Eksekusi koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
